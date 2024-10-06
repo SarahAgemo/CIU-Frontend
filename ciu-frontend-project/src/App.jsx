4a18932d9b30a4541ada86ba1bcb07af1e22581e
Sarah Agemo <160914323+SarahAgemo@users.noreply.github.com>
2024-10-06 19:22:06 +0300

    Revert "Registration"

--- ciu-frontend-project/src/App.jsx
+++ ciu-frontend-project/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './App.css';
-import Registration from './pages/admin/Registration';
+
 
 function App() {
+  
+
   return (
-    <div>
-      <Registration/>
-    </div>
-  );
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
