46c948010546167cf9669285c4fc7dae379ea2ce
NAKAWINIFRED <winifrednakawunde07@gmail.com>
2024-10-07 16:08:33 +0300

    imported registration in the app.jsx

--- ciu-frontend-project/src/App.jsx
+++ ciu-frontend-project/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login.jsx";
 // import ResetPassword from './components/admin/ResetPassword';
 import RegForm from './components/admin/RegForm';
+import Registration from "./pages/admin/Registration.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/registers" element={<Registration />} />  {/* Registration Route */}
         <Route path="/" element={<Login />} /> 
         {/* <Route path="/reset-password" element={<ResetPassword />} /> */}
         <Route path="/register" element={<RegForm />} />
