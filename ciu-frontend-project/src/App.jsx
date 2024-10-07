0e4b6fa983fd075e5e233695615ee9cf9ad3729c
Akech <akesandra659@gmail.com>
2024-10-07 11:55:54 +0300

    'student-reg'

--- ciu-frontend-project/src/App.jsx
+++ ciu-frontend-project/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login.jsx";
+// import ResetPassword from './components/admin/ResetPassword';
+import RegForm from './components/admin/RegForm';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} /> 
+        {/* <Route path="/reset-password" element={<ResetPassword />} /> */}
+        <Route path="/register" element={<RegForm />} />
+      </Routes>
+    </Router>
   );
 }
 
