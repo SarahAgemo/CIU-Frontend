a09f6f60006e08dd6b3edb2597180503af1cd270
Akech <akesandra659@gmail.com>
2024-10-12 15:45:44 +0300

    'RequestToken'

--- ciu-frontend-project/src/App.jsx
+++ ciu-frontend-project/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login.jsx";
 import ResetPassword from './components/admin/ResetPassword';
 import RegForm from './components/admin/RegForm';
+import RequestToken from './components/admin/RequestToken';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} /> 
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/register" element={<RegForm />} />
+        <Route path="/request-token" element={<RequestToken />} />
       </Routes>
     </Router>
   );
