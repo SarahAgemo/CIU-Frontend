3b7ee69b88b65985dc2c98fb3e4f6d1a757bdd63
Akech <akesandra659@gmail.com>
2024-10-12 14:57:42 +0300

    'ResetPassword'

--- ciu-frontend-project/src/App.jsx
+++ ciu-frontend-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login.jsx";
-// import ResetPassword from './components/admin/ResetPassword';
+import ResetPassword from './components/admin/ResetPassword';
 import RegForm from './components/admin/RegForm';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} /> 
-        {/* <Route path="/reset-password" element={<ResetPassword />} /> */}
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/register" element={<RegForm />} />
       </Routes>
     </Router>
