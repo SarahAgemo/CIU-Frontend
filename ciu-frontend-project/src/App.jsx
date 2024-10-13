86085903f39b175a2d78fd4d075e8c07703e5efb
katusabe scovia <scoviak588@gmail.com>
2024-10-14 01:52:41 +0300

    Your commit message here

--- ciu-frontend-project/src/App.jsx
+++ ciu-frontend-project/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login.jsx";
-// import ResetPassword from './components/admin/ResetPassword';
 import RegForm from './components/admin/RegForm';
 import Registration from "./pages/admin/Registration.jsx";
+import ManageUsers from './pages/admin/ManageUsers.jsx';
+import Dashboard from './pages/admin/Dashboard.jsx';
+import MainContent from "./components/student/MainContent.jsx";
 
 function App() {
   return (
@@ -10,8 +12,10 @@ function App() {
       <Routes>
         <Route path="/registers" element={<Registration />} />  {/* Registration Route */}
         <Route path="/" element={<Login />} /> 
-        {/* <Route path="/reset-password" element={<ResetPassword />} /> */}
         <Route path="/register" element={<RegForm />} />
+        <Route path="/manage"  element={<ManageUsers  /> } /> 
+        <Route path="/dashboard" element={<Dashboard  /> } /> 
+        <Route path="/student" element={<MainContent />} />
       </Routes>
     </Router>
   );
