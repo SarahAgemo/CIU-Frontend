2346ba67420dcf12aa7c6876ed8c7a497f3d4242
immaculate <nayigaimmaculate098@gmail.com>
2024-10-25 13:19:28 +0300

    removed redundant admin

--- ciu-frontend-project/src/App.jsx
+++ ciu-frontend-project/src/App.jsx
@@ -17,10 +17,10 @@ import Students from "./components/admin/Students.jsx";
 import EditStudent from "./components/admin/EditStudent.jsx";
 import Adminuser from "./components/admin/Adminuser.jsx";
 import Editadmin from "./components/admin/Editadmin.jsx";
-// import ResetAdminPassword from "./components/admin/ResetAdminPassword.jsx";
-// import ResetLecturerPassword from "./components/admin/ResetLecturerPassword.jsx";
-// import RequestAdminToken from "./components/admin/RequestAdminToken.jsx";
-// import RequestLecturerToken from "./components/admin/RequestLecturerToken.jsx";
+import ResetAdminPassword from "./components/admin/ResetAdminPassword.jsx";
+import ResetLecturerPassword from "./components/admin/ResetLecturerPassword.jsx";
+import RequestAdminToken from "./components/admin/RequestAdminToken.jsx";
+import RequestLecturerToken from "./components/admin/RequestLecturerToken.jsx";
 import RegCourse from "./pages/lecturer/RegCourse.jsx";
 import Courses from "./pages/lecturer/Courses.jsx";
 import EditCourse from "./pages/lecturer/EditCourses.jsx";
@@ -45,29 +45,25 @@ import ExamInstructions from "./pages/student/ExamInstructions";
 import Proctoring from "./pages/student/Proctoring";
 import Quiz from "./pages/student/Quiz";
 
-
 function App() {
   return (
     <Router>
       <Routes>
-    
         {/* Authentication */}
         <Route path="/" element={<Login />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/request-token" element={<RequestToken />} />
-        
+
         {/* Admin */}
-        <Route path="/manage" element={<ManageUsers />} /> 
+        <Route path="/manage" element={<ManageUsers />} />
         <Route path="/register" element={<RegForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/admin" element={<Dashboard />} />
         <Route path="/admin/manage-users" element={<ManageUsers />} />
         <Route path="/registers" element={<Registration />} />
-        <Route path="/manage" element={<ManageUsers />} />
         <Route path="/users" element={<Users />} />
         <Route path="/edit/:id" element={<EditUser />} />
         <Route path="/layout" element={<Layout />} />
-        <Route path="/request-token" element={<RequestToken />} />
         <Route path="/table" element={<Students />} />
         <Route path="/edit-student/:id" element={<EditStudent />} />
         <Route path="/adminuser" element={<Adminuser />} />
@@ -79,7 +75,7 @@ function App() {
         <Route path="/regCourse" element={<RegCourse />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="/editcourse/:id" element={<EditCourse />} />
-          
+
         {/* Lecturer */}
         <Route path="/schedule-upload-exams" element={<ScheduleUploadExams />} />
         <Route path="/schedule-create-exams" element={<ScheduleCreateExams />} />
@@ -89,7 +85,7 @@ function App() {
         <Route path="/exam-paper/:id/questions" element={<QuestionsPreview />} />
         <Route path="/exam-paper/:id/question/:questionId" element={<EditExamPaper />} />
         <Route path="/exam-paper/:id/edit" element={<EditExamInterface />} />
-          
+
         {/* Student */}
         <Route path="/student" element={<StudentDashboard />} />
         <Route path="/student/do-exam" element={<DoExam />} />
