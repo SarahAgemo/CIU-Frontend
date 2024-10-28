446892e570ec9cc10897c4af8d22617abf93b823
immaculate <nayigaimmaculate098@gmail.com>
2024-10-28 11:32:07 +0300

    updating my own branch

--- ciu-frontend-project/src/pages/admin/Registration.jsx
+++ ciu-frontend-project/src/pages/admin/Registration.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './Registration.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const Registration = () => {
   const navigate = useNavigate(); // Initialize useNavigate
--- ciu-frontend-project/src/pages/lecturer/Courses.jsx
+++ ciu-frontend-project/src/pages/lecturer/Courses.jsx
@@ -52,8 +52,8 @@ function UserList({ users, deleteUser }) {
     type="button"
     className="btn btn-primary"
 >
-    {/* <i className="fas fa-edit"></i>  */}
-    Edit
+    <i className="fas fa-edit"></i> 
+    {/* Edit */}
 </button>
 <button
     onClick={() => {
@@ -64,8 +64,8 @@ function UserList({ users, deleteUser }) {
     type="button"
     className="btn btn-danger"
 >
-    {/* <i className="fas fa-trash"></i>  */}
-    Delete
+    <i className="fas fa-trash"></i> 
+    {/* Delete */}
 </button>
 
                         </td>
